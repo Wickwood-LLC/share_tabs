9528db88b80894ae82859730959941d54717ee65
Junaid PV <junu.pv@gmail.com>
2025-01-13 12:03:31 +0530

    Capture and set active tab depending on hash value in the URL.

--- js/share-tabs.js
+++ js/share-tabs.js
@@ -1,13 +1,26 @@
 (function ($, Drupal, once) {
+  function clickShareTab($tab) {
+    // console.log('clicked');
+    // var $this = $(this);
+    var key = $tab.attr('data-tab-key');
+    var $share_tabs = $tab.closest('.share-tab');
+    $('.tab-content', $share_tabs).hide();
+    $('.tab-content[data-tab-key="' + key + '"]', $share_tabs).show();
+  }
   Drupal.behaviors.share_tabs = {
+
     attach: function (context, settings) {
-      $('.share-tab .tablink', context).click(function() {
-        console.log('clicked');
-        var $this = $(this);
+      var hash = window.top.location.hash.substring(1);
+      var $share_tabs_tabs = $('.share-tab .tablink', context);
+      $share_tabs_tabs.each(function() {
+        $tab = $(this);
         var key = $(this).attr('data-tab-key');
-        var $share_tabs = $this.closest('.share-tab');
-        $('.tab-content', $share_tabs).hide();
-        $('.tab-content[data-tab-key="' + key + '"]', $share_tabs).show();
+        if (hash == 'st-' + key) {
+          clickShareTab($tab);
+        }
+      });
+      $share_tabs_tabs.click(function() {
+        clickShareTab($(this));
       });
     },
   };
