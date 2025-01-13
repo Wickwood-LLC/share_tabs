117d7ac28de8e9b7b2382b210344948fe3873656
Junaid PV <junu.pv@gmail.com>
2025-01-13 13:29:06 +0530

    Consier share by URL param method.

--- js/share-tabs.js
+++ js/share-tabs.js
@@ -6,18 +6,33 @@
     var $share_tabs = $tab.closest('.share-tab');
     $('.tab-content', $share_tabs).hide();
     $('.tab-content[data-tab-key="' + key + '"]', $share_tabs).show();
+    var share_method = $share_tabs.attr('data-share-method');
+    if (share_method == 'query_param') {
+      const url = new URL(location);
+      url.searchParams.set("st", key);
+      history.pushState({}, "", url);
+    }
   }
   Drupal.behaviors.share_tabs = {
 
     attach: function (context, settings) {
       var hash = window.top.location.hash.substring(1);
+      var $share_tabs = $('.share-tab').each(function (){
+        var share_method = $(this).attr('data-share-method');
+        if (share_method == 'hash') {
+          $tabs = $('.tablink', this).each(function () {
+            $tab = $(this);
+            var key = $(this).attr('data-tab-key');
+            if (hash == 'st-' + key) {
+              clickShareTab($tab);
+            }
+          })
+        }
+        // else if (share_method == 'query_param')
+      });
       var $share_tabs_tabs = $('.share-tab .tablink', context);
       $share_tabs_tabs.each(function() {
-        $tab = $(this);
-        var key = $(this).attr('data-tab-key');
-        if (hash == 'st-' + key) {
-          clickShareTab($tab);
-        }
+        
       });
       $share_tabs_tabs.click(function() {
         clickShareTab($(this));
